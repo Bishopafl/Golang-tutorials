dcbde86659a2dfd14ffdbffb5177c376e338de11
Adam Lopez <adam.lopez@hotwiremail.com>
2025-08-14 17:32:57 -0400

    Removing other examples and moving on to just creating a new deck from the main.go file and calling the print method from the deck.go file to print each value.

--- cards/main.go
+++ cards/main.go
@@ -1,19 +1,8 @@
 package main
 
 func main() {
-
-	// Because cards is of type deck - we can call the print() method on it.
-	cards := deck{"Ace of Diamonds", newCard()}
-
-	// Important Note:
-	// takes the cards slice, add a new string to end - append() does not modify existing slice,
-	// instead it returns a new slice assigned to the variable cards
-	cards = append(cards, "Six of Hearts")
+	cards := newDeck()
 
 	cards.print()
 
-}
-
-func newCard() string {
-	return "Ace of Spades"
 }

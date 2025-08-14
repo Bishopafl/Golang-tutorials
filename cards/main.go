e374e3f15acb9f6cac6b6ef90721efd9891bd57f
Adam Lopez <adam.lopez@hotwiremail.com>
2025-08-14 10:37:36 -0400

    first commit, creating some tutorials for Go

---
+++ cards/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	cards :=
+}
+
+func newCard() float64 {
+	return 3.14
+}
---
+++ hello-world/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "fmt" // double quotes necessary
+
+func main() {
+	fmt.Println("Hi there!")
+}

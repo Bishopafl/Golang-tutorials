3159b6cc584aa074f1781859d89caa9f26145f61
Adam Lopez <adam.lopez@hotwiremail.com>
2025-08-14 17:32:18 -0400

    Creating functionality that creates a new deck and loops through the suits and values and assigns them to cards variable which is an empty instance of the deck.

--- cards/deck.go
+++ cards/deck.go
@@ -6,6 +6,23 @@ import "fmt"
 // which is a slice of strings
 type deck []string
 
+func newDeck() deck {
+	// Create an empty deck
+	cards := deck{}
+
+	cardSuits := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
+	cardValues := []string{"Ace", "Two", "Three", "Four"}
+
+	// underscores are used in go to tell it that there is a variable there but we don't care about it
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
+			cards = append(cards, value+" of "+suit)
+		}
+	}
+
+	return cards
+}
+
 // d is a receiver on a function and is similar to 'this' or 'self'
 // Basically - any variable of type "deck"
 //		now gets access to the "print" method

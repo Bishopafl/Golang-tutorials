1dcfc08c6cadc35d3622d4dda1c2836f27c749ec
Adam Lopez <adam.lopez@hotwiremail.com>
2025-08-14 17:07:24 -0400

    Creating a receiver that will be passed in the main.go file. In Go, a receiver is a special parameter in a method declaration that specifies the type on which the method is defined. *Essentially, a method in Go is a function with a receiver.*

--- cards/deck.go
+++ cards/deck.go
@@ -1,5 +1,18 @@
 package main
 
+import "fmt"
+
 // Create a new type of 'deck'
 // which is a slice of strings
 type deck []string
+
+// d is a reciever on a function and is similar to 'this' or 'self'
+// Basically - any variable of type "deck"
+//		now gets access to the "print" method
+//
+func (d deck) print() {
+
+	for i, card := range d {
+		fmt.Println(i, card)
+	}
+}

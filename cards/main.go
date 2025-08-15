81e15d704073dc470a51d3ca3072e743e96583c1
Adam Lopez <adam.lopez@hotwiremail.com>
2025-08-15 11:01:03 -0400

    Adding the print functionality to the hand and the remaining cards.

--- cards/main.go
+++ cards/main.go
@@ -3,6 +3,10 @@ package main
 func main() {
 	cards := newDeck()
 
-	cards.print()
+	hand, remainingCards := deal(cards, 5)
+
+	// both are of type deck - any value with type of deck can use this function
+	hand.print()
+	remainingCards.print()
 
 }

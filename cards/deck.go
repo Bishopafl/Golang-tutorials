09c4527589e080af7d36b65c6f787569971489e3
Adam Lopez <adam.lopez@hotwiremail.com>
2025-08-15 11:00:40 -0400

    Creating the deal functionality. the return statement has two separate ways of returning the values. this can be done like d[0:handSize] or like d[:handSize]. Go convention allows for the latter. As for the single digit variable names - this is also convention in Go to not confuse the type with the actual variable being used. Think of it like, in PHP we use 'dataTypeObject, $variableName' - in Go we use 'v dataType'

--- cards/deck.go
+++ cards/deck.go
@@ -10,12 +10,14 @@ func newDeck() deck {
 	// Create an empty deck
 	cards := deck{}
 
+	// Define and initilize card variables
 	cardSuits := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
 	cardValues := []string{"Ace", "Two", "Three", "Four"}
 
 	// underscores are used in go to tell it that there is a variable there but we don't care about it
 	for _, suit := range cardSuits {
 		for _, value := range cardValues {
+			// Append returns the updated slice. It is therefore necessary to store the result of append, often in the variable holding the slice itself.
 			cards = append(cards, value+" of "+suit)
 		}
 	}
@@ -26,10 +28,15 @@ func newDeck() deck {
 // d is a receiver on a function and is similar to 'this' or 'self'
 // Basically - any variable of type "deck"
 //		now gets access to the "print" method
-//
+// this prints to the line the deck type
 func (d deck) print() {
-
 	for i, card := range d {
 		fmt.Println(i, card)
 	}
+}
+
+// Deal functionality
+// two separate return values in return.
+func deal(d deck, handSize int) (deck, deck) {
+	return d[:handSize], d[handSize:]
 }

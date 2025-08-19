637b3a3c05224a4181481b43c7a223b863d89d37
Adam Lopez <adam.lopez@hotwiremail.com>
2025-08-19 13:35:47 -0400

    Three different ways that map can be declared in Go.

--- map/main.go
+++ map/main.go
@@ -5,10 +5,18 @@ import "fmt"
 func main() {
 	// literal syntax
 	// key values get separated with a comma - like structs!
-	colors := map[string]string{ // declaring a map with keys of type string and values of type string
-		"red":   "#FF0000",
-		"green": "#008000",
-	}
+	// Can be used with other data types as well.
+
+	// colors := map[string]string{ // declaring a map with keys of type string and values of type string
+	// 	"red":   "#FF0000",
+	// 	"green": "#008000",
+	// }
+
+	// var colors map[string]string // usually take this approach to see what we want to put into a map
+
+	colors := make(map[string]string) // makes a new map of key/values of strings ** we can add values to these if needed later
+
+	colors["white"] = "#fffff" // in maps the keys are typed, and need to be of the appropriate type
 
 	fmt.Println(colors)
 }

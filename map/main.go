8a96012018853879bb56910a6d5ffb8a0c8bff24
Adam Lopez <adam.lopez@hotwiremail.com>
2025-08-20 12:17:05 -0400

    Removing the other ways we can reference the creation of a map with key/values as strings and creating a quick loop func that will declare and assign the key/values and print them to the line. Just ways that a mapping of data can be displayed in the terminal.

--- map/main.go
+++ map/main.go
@@ -7,16 +7,20 @@ func main() {
 	// key values get separated with a comma - like structs!
 	// Can be used with other data types as well.
 
-	// colors := map[string]string{ // declaring a map with keys of type string and values of type string
-	// 	"red":   "#FF0000",
-	// 	"green": "#008000",
-	// }
+	colors := map[string]string{ // declaring a map with keys of type string and values of type string
+		"red":   "#FF0000",
+		"green": "#008000",
+		"white": "#fffff", // last property always need a comma!
+	}
 
-	// var colors map[string]string // usually take this approach to see what we want to put into a map
+	// fmt.Println(colors)
+	printMap(colors)
+}
 
-	colors := make(map[string]string) // makes a new map of key/values of strings ** we can add values to these if needed later
-
-	colors["white"] = "#fffff" // in maps the keys are typed, and need to be of the appropriate type
-
-	fmt.Println(colors)
+func printMap(c map[string]string) {
+	// loop through each map
+	// declaring, assigning the key values on :22
+	for color, hex := range c {
+		fmt.Println("Hex code for", color, "is", hex)
+	}
 }

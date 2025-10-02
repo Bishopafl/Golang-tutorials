d80d74c42b1df61416348d035a02c6ac47938cd3
Adam Lopez <adam.lopez@hotwiremail.com>
2025-10-02 12:05:20 -0400

    Adding an interface to my language bot

--- interfaces/main.go
+++ interfaces/main.go
@@ -2,6 +2,11 @@ package main
 
 import "fmt"
 
+// adding the interface type
+type bot interface {
+	getGreeting() string // takes in the getGreeting function and identifies it returns a string
+}
+
 // creating empty structs that I can set receivers up for
 type englishBot struct{}
 type spanishBot struct{}
@@ -14,14 +19,8 @@ func main() {
 	printGreeting(sb)
 }
 
-// printGreeting() has similar logic for english and spanish structs.
-
-func printGreeting(eb englishBot) {
-	fmt.Println(eb.getGreeting())
-}
-
-func printGreeting(sb spanishBot) {
-	fmt.Println(sb.getGreeting())
+func printGreeting(b bot) {
+	fmt.Println(b.getGreeting())
 }
 
 func (englishBot) getGreeting() string {
